952625373372a7920efbb119f9588e14911ae8d4
WuZhongfei-Kino <Zhongfei.Wu.Kino@aliyun.com>
2024-10-07 23:50:01 +0800

    完成：文件内容入库完成后，移动到backup文件夹下

--- src/main/java/com/wzf/thread/FileRead2DBCallable.java
+++ src/main/java/com/wzf/thread/FileRead2DBCallable.java
@@ -6,11 +6,16 @@ import com.wzf.utils.BatchNumberGenerator;
 import com.wzf.utils.JdbcUtil;
 import com.wzf.utils.ThreadLocalUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.jni.File;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.*;
@@ -41,6 +46,8 @@ public class FileRead2DBCallable implements Callable {
      */
     private CountDownLatch countDownLatch;
 
+    private String files;
+
     private String fileName;
 
     private BatchNumberGenerator batchNumberGenerator;
@@ -62,7 +69,7 @@ public class FileRead2DBCallable implements Callable {
 
     @Override
     public Object call() throws Exception {
-
+        files = "text";
         fileName = "content.txt";
         dealResultData = new DealResultData();
         String line;
@@ -73,10 +80,9 @@ public class FileRead2DBCallable implements Callable {
         AtomicInteger lineInteger = new AtomicInteger(0);
         AtomicInteger comsumeCount = new AtomicInteger();//消费总数
         AtomicInteger intoDBCount = new AtomicInteger();//入库总数
+        Path backupDirectory = Paths.get("backup");
         try (
-            BufferedReader reader = new BufferedReader(new FileReader("text/content.txt"))) {
-
-//            countDownLatch = new CountDownLatch(count);
+            BufferedReader reader = new BufferedReader(new FileReader(files + "/" + fileName))) {
             while ((line = reader.readLine()) != null) {
                 InDB inDB = new InDB();
                 inDB.setFromFile(fileName);
@@ -103,27 +109,27 @@ public class FileRead2DBCallable implements Callable {
                         log.info("任务线程：{}, 处理数量:{}", subThreadResult.getThreadName(), subThreadResult.getDealCount());
                         log.info("主线程：{} 总消费个数：{}", Thread.currentThread().getName(), comsumeCount.get());
                         log.info("主线程：{} mysql表已写入条数：{}", Thread.currentThread().getName(), intoDBCount.get());
-                        System.err.println(subThread+"==>"+this);
+//                        System.err.println(subThread+"==>"+this);
                     }
                 }
 
-                //先判断队列里元素是否有10个
-//                if (queue.size() < 10){
-//                    //未满，入队列
-//                    queue.
-//                            offer(inDB);
-//                } else {
-//                    //队列元素满10个，取元素，先把队列里的元素全部入库
-////                    queue.
-//
-////                    Object subThread = threadPoolExecutor.submit(new ImportDBCallable(jdbcUtil, queue)).get();
-////                    System.out.println("子线程执行结果:"+subThread);
-//                }
-
-//                    queue.offer()
-//                    queue.put(line); // 将行内容放入队列，如果队列满，这里会阻塞
             }
-            queue.clear();
+            //判断文件内容是否全都入库
+            if (intoDBCount.get() == lineInteger.get()){
+                //清空队列
+                queue.clear();
+                //关闭流
+                if (reader != null){
+                    reader.close();
+                }
+                log.info("主线程：{} mysql表已写入条数：{}  总生产个数：{}", Thread.currentThread().getName(), intoDBCount.get(), lineInteger);
+                //将文件移动
+                Path sourceFile = Paths.get(files+"/"+fileName);
+                Files.move(sourceFile, backupDirectory.resolve(sourceFile.getFileName()));
+                System.out.println("文件移动。。。");
+
+            }
+
             reader.close();
 //                queue.put("EOF"); // 文件读取完毕，放入结束标记
         } catch (IOException e) {
@@ -131,14 +137,6 @@ public class FileRead2DBCallable implements Callable {
             Thread.currentThread().interrupt(); // 重新中断线程
         }
 
-
-        // 以下代码用于从队列中取出内容并处理，这里简单打印出来
-        //            String item;
-//            while (!(item = queue.take()).equals("EOF")) { // 当读到EOF标记时结束循环
-//                System.out.println(item);
-//            }
-        //判断队列是否还有值
-//            while (queue.)
         return null;
 
     }
